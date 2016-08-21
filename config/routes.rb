f90031eaad6209207b8face9e4734886ed75799e
Michal Buszkiewicz <embed1@gmail.com>
2016-08-21 11:49:24 +0200

    add bootstrap, haml and simple form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
+  root to: 'pages#main'
   resources :rooms, param: :name
   resources :messages
+  resources :users do
+    collection do
+      post 'login'
+    end
+  end
 end
