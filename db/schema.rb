cc8670c7b28f3c3997959d2af6b0ef789f982857
Michal Buszkiewicz <embed1@gmail.com>
2016-08-21 11:49:50 +0200

    add uuid field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820165807) do
+ActiveRecord::Schema.define(version: 20160820202852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160820165807) do
     t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "uuid"
   end
 
   add_foreign_key "messages", "rooms"
